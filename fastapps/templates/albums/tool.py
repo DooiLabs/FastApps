a887d88a1835f172737ea40b3af91e374203e489
chaewon-huh <cw.huh@postech.ac.kr>
2025-11-14 17:52:25 +0900

    Fix albums template tool syntax

--- fastapps/templates/albums/tool.py
+++ fastapps/templates/albums/tool.py
@@ -25,69 +25,68 @@ class {ClassName}Tool(BaseWidget):
         # OpenAI guidance: inline carousels feel best with 3-8 entries,
         # so trim at the source to stay compliant when possible.
         sample_albums = [
-                {
-                    "id": "album-1",
-                    "title": "Sample Album 1",
-                    "cover": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-1.png",
-                    "photos": [
-                        {
-                            "id": "p1",
-                            "title": "Photo 1",
-                            "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-1.png"
-                        },
-                        {
-                            "id": "p2",
-                            "title": "Photo 2",
-                            "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-2.png"
-                        },
-                        {
-                            "id": "p3",
-                            "title": "Photo 3",
-                            "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-3.png"
-                        }
-                    ]
-                },
-                {
-                    "id": "album-2",
-                    "title": "Sample Album 2",
-                    "cover": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-5.png",
-                    "photos": [
-                        {
-                            "id": "p4",
-                            "title": "Photo 4",
-                            "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-4.png"
-                        },
-                        {
-                            "id": "p5",
-                            "title": "Photo 5",
-                            "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-5.png"
-                        }
-                    ]
-                },
-                {
-                    "id": "album-3",
-                    "title": "Sample Album 3",
-                    "cover": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-7.png",
-                    "photos": [
-                        {
-                            "id": "p6",
-                            "title": "Photo 6",
-                            "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-6.png"
-                        },
-                        {
-                            "id": "p7",
-                            "title": "Photo 7",
-                            "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-7.png"
-                        },
-                        {
-                            "id": "p8",
-                            "title": "Photo 8",
-                            "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-8.png"
-                        }
-                    ]
-                }
-            ]
-        }
+            {
+                "id": "album-1",
+                "title": "Sample Album 1",
+                "cover": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-1.png",
+                "photos": [
+                    {
+                        "id": "p1",
+                        "title": "Photo 1",
+                        "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-1.png"
+                    },
+                    {
+                        "id": "p2",
+                        "title": "Photo 2",
+                        "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-2.png"
+                    },
+                    {
+                        "id": "p3",
+                        "title": "Photo 3",
+                        "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-3.png"
+                    }
+                ]
+            },
+            {
+                "id": "album-2",
+                "title": "Sample Album 2",
+                "cover": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-5.png",
+                "photos": [
+                    {
+                        "id": "p4",
+                        "title": "Photo 4",
+                        "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-4.png"
+                    },
+                    {
+                        "id": "p5",
+                        "title": "Photo 5",
+                        "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-5.png"
+                    }
+                ]
+            },
+            {
+                "id": "album-3",
+                "title": "Sample Album 3",
+                "cover": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-7.png",
+                "photos": [
+                    {
+                        "id": "p6",
+                        "title": "Photo 6",
+                        "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-6.png"
+                    },
+                    {
+                        "id": "p7",
+                        "title": "Photo 7",
+                        "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-7.png"
+                    },
+                    {
+                        "id": "p8",
+                        "title": "Photo 8",
+                        "url": "https://pub-d9760dbd87764044a85486be2fdf7f9f.r2.dev/example-8.png"
+                    }
+                ]
+            }
+        ]
 
         return {
             "albums": sample_albums[:8]
